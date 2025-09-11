70f4c62a87a8aba21e1a7752bb1353f29d252de0
Heloisa <helonogalcabrera@gmail.com>
2025-09-11 20:38:34 -0300

    alteração service

--- src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
@@ -1,10 +1,45 @@
 package com.example.crudSpring.projetoCRUD.SERVICE;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
+import com.example.crudSpring.projetoCRUD.REPOSITORY.EmpresaRepository;
 
 @Service
 public class EmpresaService {
 
+    private EmpresaRepository empresaRepository;
+
+    public EmpresaService(EmpresaRepository ligacaoEmpresaRepository){
+
+        empresaRepository = ligacaoEmpresaRepository;
+    }
+
+    public List<Empresa> findAll() {
+
+        return empresaRepository.findAll();
+        //select * from empresa                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    }
+
+    public Empresa cadastrarEmpresa(Empresa dadosEmpresa){
+        return empresaRepository.save(dadosEmpresa);
+    }
+
+    public void deletarEmpresa(Empresa dadosEmpresa){
+        empresaRepository.delete(dadosEmpresa);
+    }
     
+
+
+    //SELECT OU INSERTS OU ALTER TABLE OU DELETE
+
+    // SELECT * FROM EMPRESA (SELECT REALIZA LISTAGEM DE DADOS)
+    // where nome_empresa = ?
+    //retorna todos os dados da empresa (* - tudo da tabela)
+
+
+
     
 }
